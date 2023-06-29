4f8181ca5d9d2e41b09cf0117a811d02467465a5
Rodrigo Lins <rodrigolins@pm.me>
2023-06-29 17:52:38 -0300

    DS: criando formerror

---
+++ Inputs/Form/FormError.vue
@@ -0,0 +1,28 @@
+<script setup>
+import Text3Component from '../../Typography/Text/Text3Component.vue';
+</script>
+
+<template>
+  <div class="form-error">
+    <Text3Component>
+      <slot></slot>
+    </Text3Component>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.form-error {
+  display: inline-flex;
+  padding: 4px;
+  align-items: flex-start;
+  gap: 10px;
+  width: fit-content;
+
+  flex: 0;
+
+  text-align: left;
+
+  background-color: var(--red-1-low-opacity);
+  color: var(--red-1);
+}
+</style>
