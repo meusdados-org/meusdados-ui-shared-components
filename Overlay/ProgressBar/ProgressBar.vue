4ea6cb312ba3a03c1192adb480ef919bee8a1ce2
Rodrigo Lins <rodrigolins@pm.me>
2023-08-30 09:24:01 -0300

    MD-412: progressbar

---
+++ Overlay/ProgressBar/ProgressBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+  totalSteps: {
+    type: Number,
+    required: true,
+  },
+  currentStep: {
+    type: Number,
+    required: true,
+  },
+});
+
+const progress = computed(() => {
+  return (props.currentStep / props.totalSteps) * 100;
+});
+</script>
+
+<template>
+  <div class="progress-bar">
+    <div class="progress-bar__bar" :style="{ width: progress + '%' }"></div>
+  </div>
+</template>
+
+<style scoped>
+.progress-bar {
+  width: 100%;
+  height: 4px;
+  background-color: transparent;
+  border-radius: 4px;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+}
+
+.progress-bar__bar {
+  height: 100%;
+  background-color: var(--purple-1);
+  transition: width 0.3s ease-in-out;
+}
+</style>
