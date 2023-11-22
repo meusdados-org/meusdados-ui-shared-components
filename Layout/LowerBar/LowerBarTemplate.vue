0b7661ceaa21870f5b75694014731e2db4bab294
Rodrigo Lins <rodrigolins@pm.me>
2023-11-22 20:22:28 -0300

    MD-453: footer changes

---
+++ Layout/LowerBar/LowerBarTemplate.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ButtonLink from '../../Actions/ButtonLink.vue';
+import LinkComponent from '../../Actions/Link/LinkComponent.vue';
+
+</script>
+
+<template>
+  <div class="lower-bar">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.lower-bar {
+  padding: var(--spacing-small) var(--spacing-large);
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: transparent;
+  box-shadow: var(--shadow-small);
+}
+</style>
